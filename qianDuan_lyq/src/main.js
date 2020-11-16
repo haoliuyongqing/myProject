2ea97fc63ccdeb4fde508510be3c0b535aaa28d4
liuyongqing <haoliuyongqing@163.com>
2020-11-16 15:18:40 +0800

    11

--- qianDuan_lyq/src/main.js
+++ qianDuan_lyq/src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex/index'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 // import PersonalInfo from './moudules/personalInfo/pages/personalInfo'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
--- qianDuan_lyq/src/moudules/personalInfo/pages/personalInfo.vue
+++ qianDuan_lyq/src/moudules/personalInfo/pages/personalInfo.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
+    <div class="class1">
         <h3>欢迎来到信息修改页面</h3>
-        <div>{{msg}}</div>
+        <div class="nameInput"> 姓名：<el-input v-model="input" placeholder="请输入姓名" clearable></el-input></div>
+        <div class="sex-input"> 姓别：
+            <el-radio v-model="radio" label="1">男</el-radio>
+            <el-radio v-model="radio" label="2">女</el-radio>
+        </div>
+        <div class="phonIinput"> 手机号码：<el-input  v-model="input" placeholder="请输入号码" clearable></el-input></div>
+        <div class="idNumIinput">
+          <el-input placeholder="请输入证件号码" v-model="idNum" class="input-with-select">
+            <el-select class="selectType" v-model="idType" slot="prepend" placeholder="请选择">
+              <el-option label="身份证号" value="1"></el-option>
+              <el-option label="护照" value="2"></el-option>
+              <el-option label="港澳通行证" value="3"></el-option>
+            </el-select>
+          </el-input>
+        </div>
     </div>
 </template>
 
@@ -10,26 +24,29 @@ export default {
     name: 'personalInfo',
   data () {
     return {
-      msg: '哈哈哈 我懂了'
+      input: '哈哈哈 我懂了',
+      radio:'1',
+      idType: '',
+      idNum: '',
+
     }
   },
   mounted () {
-    var person2 = {
-    name:'李四',
-    age:18,
-    fav: ()=>{
-         // 当前this指向了定义时所在的对象（window）
-         console.log(this);  // window
-    }
-
-  }
-  person2.fav()
-  console.log(this.$store.state.IDCardInfo.NAME)
-
-
+  
   }
 }
 </script>
-<style scoped>
 
+<style>
+  .input-with-select {
+    background-color: #fff;
+    width: 600px;
+  
+  }
+    .idNumIinput{
+      margin-top: 100px !important;
+    }
+    .selectType{
+      width: 200px;
+    }
 </style>
